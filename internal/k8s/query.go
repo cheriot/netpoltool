962dc84293183a5cfe277212fd38fa76cbcbac1f
Chris Heriot <cheriot@gmail.com>
2022-01-03 14:37:45 -0800

    Import k8s auth providers

--- internal/k8s/query.go
+++ internal/k8s/query.go
@@ -10,6 +10,7 @@ import (
 	nwv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	restclient "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
